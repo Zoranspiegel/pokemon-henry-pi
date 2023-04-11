c5eb9a883d8fbd0376b751e0a1d9130c7f163b88
Juan José Rodriguez <zoranbow@gmail.com>
2023-04-10 21:19:35 -0500

    add/sequelize models & relations

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: false
     },
-  });
+    health: {
+      type: DataTypes.INTEGER,
+      defaultValue: 100,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      defaultValue: 50,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      defaultValue: 50
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      defaultValue: 50
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      defaultValue: 5
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      defaultValue: 40
+    }
+  }, { timestamps: false });
 };
