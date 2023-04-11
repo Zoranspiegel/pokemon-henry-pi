21fa8f68ea51599a7bc4b2dddd724debe0de5e53
Juan José Rodriguez <zoranbow@gmail.com>
2023-04-10 22:34:57 -0500

    add/routes/rtGetTypes

--- api/src/routes/rtTypes.js
+++ api/src/routes/rtTypes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const mwSaveTypes = require('../middlewares/mwSaveTypes');
+const getTypes = require('../controllers/getTypes');
 
 const router = Router();
 
-router.get('/', mwSaveTypes);
+router.get('/', mwSaveTypes, getTypes);
 
 module.exports = router;
