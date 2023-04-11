4e76ec030406de150b96b8b4d9dab9ae62809d54
Juan José Rodriguez <zoranbow@gmail.com>
2023-04-10 22:14:39 -0500

    add/routes/rtTypes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const rtPokemons = require("./rtPokemons");
+const rtPokemons = require('./rtPokemons');
+const rtTypes = require('./rtTypes');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/pokemons", rtPokemons);
+router.use('/pokemons', rtPokemons);
+router.use('/types', rtTypes);
 
 
 module.exports = router;
