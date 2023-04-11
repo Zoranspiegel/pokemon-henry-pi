4ecfd880ca530e56fb870a26dc2a7133d66fc07a
Juan José Rodriguez <zoranbow@gmail.com>
2023-04-10 22:38:59 -0500

    update/controllers/createPokemon(data relation)

--- api/src/controllers/createPokemon.js
+++ api/src/controllers/createPokemon.js
@@ -4,6 +4,8 @@ module.exports = async (req, res) => {
   try {
     //Uso el método 'create' del modelo 'Pokemon' para crear una nueva entidad pasando el body como argumento
     const newPokemon = await Pokemon.create(req.body);
+    //Uso el método 'setTypes' del modelo 'Pokemon' para pasarle las foreign keys por body y establecer relación con el modelo 'Type'
+    await newPokemon.setTypes(req.body.types);
     //Respuesta OK
     res.status(201).json({
       message: 'Pokemon successfully created',
